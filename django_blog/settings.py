952d09c760745665c4412aaea832de4244311e6c
Mike Zhang <zhangmike1999@gmail.com>
2024-03-09 18:48:36 -0500

    view

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # mine
+    'blog.apps.BlogConfig',
+    # build in
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
